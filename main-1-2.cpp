6015a78c4587f7595158d793b217bff27fa107f5
aaron-morley <111508607+aaron-morley@users.noreply.github.com>
2023-03-24 23:39:46 +1030

    Add files via upload

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -10,15 +10,15 @@
 int main()
 	{
 		int n = 4;
-		PersonList personList = createPersonList(n);
-		std::cout << "Person list - numPersons: " << personList.numPersons << std::endl;
+		PersonList pList = createPersonList(n);
+		std::cout << "Person list - numPersons: " << pList.numPersons << std::endl;
   
 		for (int i = 0; i < n; ++i)
 			{
-				std::cout << "Person: " << i+1 << "Name: " << personList.persons[i].name << ", Age: " << personList.persons[i].age << std::endl;
+				std::cout << "Person: " << i+1 << "Name: " << pList.persons[i].name << ", Age: " << pList.persons[i].age << std::endl;
 			}
 			
-	delete[] personList.persons;
+	delete[] pList.persons;
   
 	return 0;
   
