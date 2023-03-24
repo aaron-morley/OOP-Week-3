2a8131305ac5e73c1738310537a9eff07244e78f
aaron-morley <111508607+aaron-morley@users.noreply.github.com>
2023-03-24 23:48:30 +1030

    Update main-1-2.cpp

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -11,14 +11,14 @@ int main()
 	{
 		int n = 4;
 		PersonList pList = createPersonList(n);
-		std::cout << "Person list - numPersons: " << pList.numPersons << std::endl;
+		std::cout << "Person list - numPeople: " << pList.numPeople << std::endl;
   
 		for (int i = 0; i < n; ++i)
 			{
-				std::cout << "Person: " << i+1 << "Name: " << pList.persons[i].name << ", Age: " << pList.persons[i].age << std::endl;
+				std::cout << "Person: " << i+1 << "Name: " << pList.people[i].name << ", Age: " << pList.people[i].age << std::endl;
 			}
 			
-	delete[] pList.persons;
+	delete[] pList.people;
   
 	return 0;
   
