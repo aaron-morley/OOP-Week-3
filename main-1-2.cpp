265652e7abbd7342ee86bfc1f22c6e171354d4e4
aaron-morley <111508607+aaron-morley@users.noreply.github.com>
2023-03-24 23:42:34 +1030

    Add files via upload

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,25 +1,14 @@
-// main-1-2.cpp
-
 #include <iostream>
-#include <string>
-
 #include "Person.h"
 
-
-
-int main()
-	{
-		int n = 4;
-		PersonList pList = createPersonList(n);
-		std::cout << "Person list - numPersons: " << pList.numPersons << std::endl;
-  
-		for (int i = 0; i < n; ++i)
-			{
-				std::cout << "Person: " << i+1 << "Name: " << pList.persons[i].name << ", Age: " << pList.persons[i].age << std::endl;
-			}
-			
-	delete[] pList.persons;
-  
-	return 0;
-  
+int main() {
+  int n = 3;
+  PersonList pl = createPersonList(n);
+  std::cout << "Person list - numPeople: " << pl.numPeople << std::endl;
+  for (int i = 0; i < n; ++i) {
+    std::cout << "Person " << i+1 << " - Name: " << pl.people[i].name << ", Age: " << pl.people[i].age << std::endl;
+  }
+  delete[] pl.people;
+  return 0;
 }
+
